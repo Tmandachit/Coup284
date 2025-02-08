1f8d440164037e31d9b34ba7660492122303452e
Ty Mandachit <tmandachit@gmail.com>
2025-02-07 19:35:05 -0600

    Basic connection from front to back

---
+++ client/src/Components/HomePage.js
@@ -0,0 +1,14 @@
+import {Component} from 'react';
+
+class HomePage extends Component {
+    render() { 
+        return (  
+            <div className="HomeContainer">
+                <h1>Home Page</h1>
+                <p1>Test Text</p1>
+            </div>
+        );
+    }
+}
+ 
+export default HomePage;
